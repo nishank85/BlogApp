ea18848b55711fb881acd73d757af23e31f405cf
Nishank <nishank@idyllic-software.com>
2012-11-08 19:21:40 +0530

    Update config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 UserAuth::Application.configure do
   config.cache_classes = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.compress = true
