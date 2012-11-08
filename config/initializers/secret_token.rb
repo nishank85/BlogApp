2a610a27e05e95d95b4a095f1b65dd07d0f7277b
Nishank <nishank@idyllic-software.com>
2012-11-08 19:47:27 +0530

    Update config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-UserAuth::Application.config.secret_token = '3b8b9c32e4045a0c638bceb4ab3456d6f6820ce67428c2576dfc0a69f671bea8fa2ddf54ad42fe9b0d202ff271a8644613a9000173ee8948e3ceb4c4c0e4d3f8'
+BlogApp::Application.config.secret_token = '3b8b9c32e4045a0c638bceb4ab3456d6f6820ce67428c2576dfc0a69f671bea8fa2ddf54ad42fe9b0d202ff271a8644613a9000173ee8948e3ceb4c4c0e4d3f8'
