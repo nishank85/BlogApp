b46fd8d8ad16acf8d8af7334d93eacd93ef03014
Nishank <nishank@idyllic-software.com>
2012-11-08 19:46:15 +0530

    Update config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-UserAuth::Application.configure do
+BlogApp::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
