684bece72a5279f3ec1ffc4cdde2f6e0157ce9af
Nishank <nishank@idyllic-software.com>
2012-11-08 19:19:02 +0530

    Update config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,37 +1,29 @@
 UserAuth::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-
   # Change mail delivery to either :smtp, :sendmail, :file, :test
   config.action_mailer.delivery_method = :smtp
-  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "test.app.rubyonrails@gmail.com",
-    password: "t$s1rb4rl7ng9sd3i#sl"
+    user_name: "email",
+    password: "password"
   }
-
   # specify what domain to use for mailer URLs
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
